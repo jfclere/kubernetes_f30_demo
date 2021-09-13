0dc0e5abc43e7d5d4b7d4b3ba2ea2159910e7a6c
root <root@bela1.gva.redhat.com>
2021-09-13 18:32:23 +0200

    prepare master for my lab nodes...

--- start_master.sh
+++ start_master.sh
@@ -1,24 +1,3 @@
-ping -c 1 -w 10 green
-if [ $? -ne 0 ]; then
-  echo "Node: green not up, please check..."
-  exit 1
-fi
-ping -c 1 -w 10 blue
-if [ $? -ne 0 ]; then
-  echo "Node: blue not up, please check..."
-  exit 1
-fi
-ping -c 1 -w 10 master
-if [ $? -ne 0 ]; then
-  echo "Node: master (self) not up, please check..."
-  exit 1
-fi
-myhost=`hostname`
-if [ $myhost != "master" ]; then
-  echo "Not on master, giving up..."
-  exit 1
-fi
-
 # make sure to have entropy...
 service rngd start
 #/sbin/rngd -f -r /dev/urandom -o /dev/random
@@ -32,16 +11,22 @@ iptables -A INPUT -p tcp --dport 6443 -m conntrack --ctstate NEW,ESTABLISHED -j 
 iptables -A OUTPUT -p tcp --sport 6443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 swapoff --all
 modprobe br_netfilter
+modprobe overlay
+echo "1" > /proc/sys/net/bridge/bridge-nf-call-iptable
 
 # start new one.
 echo ""
 echo "starting with kubeadm init!!!"
 echo ""
 #kubeadm init --token=102952.1a7dd4cc8d1f4cc5 --kubernetes-version $(kubeadm version -o short)
-kubeadm init
+kubeadm init --config kubeadm-config.yaml
+
 rm -rf $HOME/.kube
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl get nodes
-kubectl apply -f weave-kube.yaml
+
+#Network 10.32.0.0/12 overlaps with existing route 10.33.144.0/24 on host
+#  kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=10.32.0.0/16"
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=10.32.0.0/16"
