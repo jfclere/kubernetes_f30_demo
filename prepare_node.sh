c01bf799a89a8c05a483f2d08d974c1ecc0285f0
root <root@bela1.gva.redhat.com>
2021-09-13 11:43:20 +0200

    Add a prepare node for lab boxes.

---
+++ prepare_node.sh
@@ -0,0 +1,9 @@
+kubeadm reset -f
+systemctl stop firewalld
+iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
+ipvsadm -C
+iptables -A INPUT -p tcp --dport 6443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 6443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+swapoff --all
+modprobe br_netfilter
+echo "1" > /proc/sys/net/bridge/bridge-nf-call-iptables
